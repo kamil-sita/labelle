bff5f7a1d51d9cea5d5f3bcba162f351903f4cc7
Sita <kamilsitarski98@gmail.com>
2024-05-19 19:54:52 +0200

    Improve error handling when unknown filename

--- gui-local/src/main/java/place/sita/labelle/gui/local/fx/FxSceneBuilder.java
+++ gui-local/src/main/java/place/sita/labelle/gui/local/fx/FxSceneBuilder.java
@@ -12,6 +12,19 @@ public class FxSceneBuilder {
     private static final Logger log = LoggerFactory.getLogger(FxSceneBuilder.class);
 
     public static Node setupFxView(Object controller, String resource) {
+        try {
+            return getParent(controller, resource);
+        } catch (Exception e) {
+            throw new SetupException(getDetails(controller, resource) + ". Does file \"" + resource + "\" exist?", e);
+        }
+    }
+
+    private static String getDetails(Object controller, String resource) {
+        return "Exception when creating for controller: \"" + controller.getClass().getName() +
+                "\" and resource \"" + resource + "\"";
+    }
+
+    private static Parent getParent(Object controller, String resource) {
         Class<?> clazz = controller.getClass();
         log.debug("[{}], Setting up FX view", clazz);
         log.debug("[{}] Creating parent loader", clazz);
@@ -23,8 +36,16 @@ public class FxSceneBuilder {
             root = loader.load();
             log.debug("[{}] Parent loaded", clazz);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new SetupException(getDetails(controller, resource), e);
+        } catch (Exception e) {
+            throw new SetupException(getDetails(controller, resource) + ". Does file \"" + resource + "\" exist?", e);
         }
         return root;
+    }
+
+    public static class SetupException extends RuntimeException {
+        public SetupException(String message, Throwable cause) {
+            super(message, cause);
+        }
     }
 }
