3d2e054d7ff1862af99cf3adf9e591ff59c05764
Sita <kamilsitarski98@gmail.com>
2024-06-29 05:40:12 +0200

    remove selected dead code

--- core/src/main/java/place/sita/labelle/core/repository/inrepository/Marker.java
+++
@@ -1,8 +0,0 @@
-package place.sita.labelle.core.repository.inrepository;
-
-import java.util.UUID;
-
-public record Marker(UUID id, String value, String category, boolean shared) {
-
-
-}
--- core/src/main/java/place/sita/labelle/core/repository/inrepository/Tag.java
+++
@@ -1,6 +0,0 @@
-package place.sita.labelle.core.repository.inrepository;
-
-import java.util.UUID;
-
-public record Tag(UUID id, String value, String category) {
-}
