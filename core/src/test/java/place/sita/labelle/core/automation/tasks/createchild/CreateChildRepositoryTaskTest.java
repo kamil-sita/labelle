0e313816a4f362a903ede6675d928903740cb48b
Sita <kamilsitarski98@gmail.com>
2024-07-28 23:58:16 +0200

    #86: Fix broken test

--- core/src/test/java/place/sita/labelle/core/automation/tasks/createchild/CreateChildRepositoryTaskTest.java
+++ core/src/test/java/place/sita/labelle/core/automation/tasks/createchild/CreateChildRepositoryTaskTest.java
@@ -71,7 +71,7 @@ public class CreateChildRepositoryTaskTest extends TestContainersTest {
 		List<Repository> repos = repositoryService.getRepositories();
 		assertThat(repos).hasSize(2);
 		assertThat(repos.stream().map(Repository::name).toList()).contains("New repo name");
-		var images = (inRepositoryService.images().images()).process().filterByRepository(results.result()).getAll();
+		var images = inRepositoryService.images().images().process().filterByRepository(results.result()).getAll();
 		assertThat(images).hasSize(1);
 		var tags = inRepositoryService.getTags(images.get(0).id());
 		assertThat(tags).hasSize(1);
@@ -111,7 +111,7 @@ public class CreateChildRepositoryTaskTest extends TestContainersTest {
 		List<Repository> repos = repositoryService.getRepositories();
 		assertThat(repos).hasSize(3);
 		assertThat(repos.stream().map(Repository::name).toList()).contains("New repo name");
-		var images = (inRepositoryService.images().images()).process().filterByRepository(results.result()).getAll();
+		var images = inRepositoryService.images().images().process().filterByRepository(results.result()).getAll();
 		assertThat(images).hasSize(1);
 		var tags = inRepositoryService.getTags(images.get(0).id());
 		assertThat(tags).hasSize(2);
@@ -146,7 +146,7 @@ public class CreateChildRepositoryTaskTest extends TestContainersTest {
 		List<Repository> repos = repositoryService.getRepositories();
 		assertThat(repos).hasSize(2);
 		assertThat(repos.stream().map(Repository::name).toList()).contains("New repo name");
-		var images = (inRepositoryService.images().images()).getAll();
+		var images = inRepositoryService.images().images().process().filterByRepository(results.result()).getAll();
 		assertThat(images).isEmpty();
 	}
 
